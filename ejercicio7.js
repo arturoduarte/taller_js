bc2acabd1834a54e6d45d5c41ba378336523538b
arturo <informa.fj@gmail.com>
2018-03-16 22:05:00 -0300

    Primer commit

---
+++ ejercicio7.js
@@ -0,0 +1,53 @@
+var asdf = [{
+	"completed":false,
+	"id":1,
+	"title":"Delectus aut autem",
+},
+{
+	"completed":false,
+	"id":2,
+	"title":"Quis ut nam facilis et officia qui",
+},
+{
+	"completed":false,
+	"id":3,
+	"title":"Fugiat veniam minus",
+},
+{
+	"completed":true,
+	"id":4,
+	"title":"Et porro tempora",
+},
+{
+	"completed":false,
+	"id":5,
+	"title":"Laboriosam mollitia et enim quasi adipisci quia provident illum"
+	,
+},
+{
+	"completed":false,
+	"id":6,
+	"title":"Qui ullam ratione quibusdam voluptatem quia omnis",
+},
+{
+	"completed":false,
+	"id":7,
+	"title":"Illo expedita consequatur quia in",
+	"userId":1
+},
+{
+	"completed":true,
+	"id":8,
+	"title":"Quo adipisci enim quam ut ab",
+},
+{
+	"completed":false,
+	"id":9,
+	"title":"Molestiae perspiciatis ipsa",
+},
+{
+	"completed":true,
+	"id":10,
+	"title":"Illo est ratione doloremque quia maiores aut",
+}];
+
